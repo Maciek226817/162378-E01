5a438cb067ea189dada0ac7a853e145f26d8a4cc
Maciek226817 <sobmaciek123456789@gmail.com>
2022-01-31 13:42:36 +0100

    Initial commit

---
+++ src/Z1.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Z1 {
+    public static <T>ArrayList<T> removeReapeatedElements(ArrayList<T> ar){
+        ArrayList<T> res = new ArrayList<>();
+
+    }
+}
---
+++ src/Z2.java
@@ -0,0 +1,6 @@
+public class Z2 {
+    public static void main(String[] args) {
+
+    }
+    public static <E> void printMarginal(Iterable<E> )
+}
